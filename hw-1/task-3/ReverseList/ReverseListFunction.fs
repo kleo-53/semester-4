772461a19c849625d179245f859c3457d8481163
kleo-53 <liza.1610@mail.ru>
2023-02-24 13:48:28 +0400

    List reversing function

---
+++ hw-1/task-3/ReverseList/ReverseListFunction.fs
@@ -0,0 +1,18 @@
+﻿// Reverse list function
+let reverse list = 
+    let rec reverseList givenList resultList =
+        if givenList = [] then resultList 
+        else
+            let resultList = givenList.Head :: resultList
+            reverseList givenList.Tail resultList
+    reverseList list []
+    
+printf "Enter elements of the list in one line: "
+let inputLine = System.Console.ReadLine()
+if inputLine = "" then printf "Can not reverse list without elements"
+else
+    let inputLine = inputLine.Split(" ")
+    let inputList = List.init inputLine.Length (fun i -> inputLine[i] |> int)
+    let result = reverse inputList
+    printf "Reversed list is: "
+    result |> Seq.iter (printf "%d ")
