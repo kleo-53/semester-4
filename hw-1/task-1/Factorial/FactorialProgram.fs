20124d5da2bc7254c0fe889e4952ad1609b08b98
kleo-53 <liza.1610@mail.ru>
2023-02-24 14:09:24 +0400

    Add tests

---
+++ hw-1/task-1/Factorial/FactorialProgram.fs
@@ -0,0 +1,12 @@
+﻿// Counting factorial by number
+let rec factorial number =
+    if number < 0 then -1
+    else if number = 1 || number = 0 then 1
+    else
+        number * factorial (number - 1)
+
+printf "Enter number to count factorial: "
+let number = System.Console.ReadLine() |> int
+let result = factorial number
+if result = -1 then printf "Can not count factorial by negative number"
+else printf $"Factorial of {number} is {result}"
