e77d262e538d0c90acd19ace34868583bbf587a9
kleo-53 <liza.1610@mail.ru>
2023-02-28 02:15:07 +0400

    Fix pattern-matching in output

--- hw-1/task-3/ReverseList/ReverseListFunction.fs
+++ hw-1/task-3/ReverseList/ReverseListFunction.fs
@@ -10,8 +10,9 @@ let reverse list =
     
 printf "Enter elements of the list in one line: "
 let inputLine = System.Console.ReadLine()
-if inputLine = "" then printf "Can not reverse list without elements"
-else
+match inputLine with
+    | "" -> printf "Can not reverse list without elements"
+    | inputLine ->
     let inputLine = inputLine.Split(" ")
     let inputList = List.init inputLine.Length (fun i -> inputLine[i] |> int)
     let result = reverse inputList
