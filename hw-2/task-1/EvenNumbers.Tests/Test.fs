bda784970d9049c3818d15edcc59b21e388c2aa5
kleo-53 <liza.1610@mail.ru>
2023-03-11 22:09:43 +0400

    Add tests

--- hw-2/task-1/EvenNumbers.Tests/Test.fs
+++ hw-2/task-1/EvenNumbers.Tests/Test.fs
@@ -2,10 +2,22 @@ module EvenNumbers.Tests
 
 open FsCheck
 open EvenFunctions
+open NUnit.Framework
+open FsUnit
 
 let EqualFunctionsTest (testList:list<int>) = (evenMap testList = evenFilter testList && evenFilter testList = evenFold testList)
 
-Check.Quick EqualFunctionsTest
+Check.QuickThrowOnFailure EqualFunctionsTest
 
-let revIsOrig (xs:list<int>) = List.rev xs = xs
-Check.QuickThrowOnFailure revIsOrig
+let EvenFunctionsTestCaseData () =
+    [ 
+        [], 0
+        [ 1; 2; 3 ], 1
+        [ -2; 2; 0 ], 3
+        [ 1; 3; 5 ; 7; 9; -1], 0 
+        [1], 0
+    ] |> List.map (fun (testList, result) -> TestCaseData(testList, result))
+
+[<TestCaseSource(nameof EvenFunctionsTestCaseData)>]
+let evenNumbersTest testList correctResult =
+    evenMap testList |> should equal correctResult
--- hw-2/task-1/EvenNumbers/EvenFunctions.fs
+++ hw-2/task-1/EvenNumbers/EvenFunctions.fs
@@ -1,14 +1,18 @@
-﻿let evenMap list = 
+﻿// Finds even numbers using map function
+let evenMap list = 
     List.map (fun x -> if x % 2 = 0 then 1 else 0 ) list |> List.sum
 
+// Finds even numbers using filter function
 let evenFilter list = 
     List.filter (fun x -> x % 2 = 0) list |> List.length
 
+// Finds even numbers using fold function
 let evenFold list = 
     List.fold (fun acc x -> if x % 2 = 0 then acc + 1 else acc) 0 list
     
+printf "Enter list in one line: "
 let inputLine = System.Console.ReadLine()
 let inputLinee = inputLine.Split(" ")
 let inputList = List.init inputLinee.Length (fun i -> inputLinee[i] |> int)
 let result = evenFold inputList
-printfn $"{result}"
+printfn $"List contains {result} even numbers"
