8ae7612cb19bc156b30efcc4715c43b629ccb558
kleo-53 <liza.1610@mail.ru>
2023-04-03 20:47:22 +0400

    PointFree function

--- hw-4/task-2/PointFree/Program.fs
+++ hw-4/task-2/PointFree/Program.fs
@@ -1,1 +1,15 @@
-﻿
+﻿open FsCheck
+
+let func x l = List.map (fun y -> y * x) l
+
+let func1 x : (int) list -> (int) list =
+    List.map (fun y -> x * y)
+
+let func2 x : (int) list -> (int) list =
+    List.map ((*) x)
+    
+let func3 : int -> (int) list -> (int) list =
+    List.map << (*)
+
+let checking (x:int) (l:list<int>) = (func x l) = (func3 x l) 
+Check.QuickThrowOnFailure checking 
