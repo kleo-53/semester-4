e1bc4f037862c0c1e77e11b6e8adcb17787a825a
kleo-53 <liza.1610@mail.ru>
2023-04-11 18:15:39 +0400

    Add missing files

---
+++ hw-3/task-3/LambdaInterpreter/Program.fs
@@ -0,0 +1,17 @@
+﻿type Term =
+    | Variable of char
+    | Application of Term * Term
+    | Abstraction of char * Term
+
+module Interpreter =
+    let GetFreeVariables lambdaTerm =
+        let FreeVarSet = Set.empty
+        let rec loop lambdaTerm =
+            match lambdaTerm with
+            | Variable v -> FreeVarSet.Add v
+            | Application (termA,  termB) -> (loop termA) + (loop termB)
+            | Abstraction (v, term) -> (FreeVarSet.Remove v) + (loop term)
+        loop lambdaTerm
+
+    let betaReduction =
+
