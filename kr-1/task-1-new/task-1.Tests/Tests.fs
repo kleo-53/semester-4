a0547f1cb8417371cfff50505109beae4b4a0618
Elizaveta Karaseva <liza.1610@mail.ru>
2023-04-21 14:02:04 +0400

    recreate task-1

--- kr-1/task-1-new/task-1.Tests/Tests.fs
+++ kr-1/task-1-new/task-1.Tests/Tests.fs
@@ -1,8 +1,10 @@
 module task_1.Tests
 
 open NUnit.Framework
+open task_1
+open FsUnit
 
 [<Test>]
 let Test1 () =
     let testList = [sin 1.0; cos 1.0; sin 2.0; cos 2.0; sin 3.0; cos 3.0]
-    supermap [1.0; 2.0; 3.0] (fun x -> [sin x; cos x]) should equal testList
+    superMap.supermap [1.0; 2.0; 3.0] (fun x -> [sin x; cos x]) |> should equal testList
