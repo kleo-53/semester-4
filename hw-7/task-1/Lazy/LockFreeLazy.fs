329699ab3c955890487068997bbb31799aae8668
Elizaveta Karaseva <liza.1610@mail.ru>
2023-05-26 03:02:08 +0400

    Fix lazy realizations; add tests

--- hw-7/task-1/Lazy/LockFreeLazy.fs
+++ hw-7/task-1/Lazy/LockFreeLazy.fs
@@ -4,16 +4,10 @@ open System.Threading
 
 type LockFreeLazy<'a> (supplier : unit -> 'a) =
     let mutable result = None
-    let mutable currentVal = None
-    let mutable startVal = result
-    let mutable isDone = false
 
     interface ILazy<'a> with
         member this.Get() =
             if result.IsNone then 
-                let current = result
                 let calc = Some (supplier())
-                while not <| obj.ReferenceEquals(result, Interlocked.CompareExchange(&result, calc, current)) do
-                    let current = result
-                    let calc = Some (supplier())
+                Interlocked.CompareExchange(&result, calc, None) |> ignore
             result.Value
--- hw-7/task-1/Lazy/ThreadsLazy.fs
+++ hw-7/task-1/Lazy/ThreadsLazy.fs
@@ -4,11 +4,15 @@ open System
 
 type ThreadsLazy<'a> (supplier : unit -> 'a) =
     [<VolatileField>]
-    let mutable result = None
+    let mutable result: 'a Option = None
     let lockObj = new Object()
 
     interface ILazy<'a> with
         member this.Get() =
             if result.IsNone then 
-                lock lockObj (fun () -> result <-Some (supplier()))
-            result.Value
+                lock lockObj (fun () ->
+                    if result.IsNone then
+                        result <- Some (supplier())
+                    result.Value)
+            else
+                result.Value
