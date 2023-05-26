a9b9711309168dccd23d2a6240a15c3182529c2a
Elizaveta Karaseva <liza.1610@mail.ru>
2023-05-26 19:17:12 +0400

    Fix mutable map

--- hw-5/task-1.Tests/Tests.fs
+++ hw-5/task-1.Tests/Tests.fs
@@ -7,15 +7,17 @@ open FsUnit
 
 [<Test>]
 let Test1 () =
-    let computers = [Computer(0, Windows, false);
-    Computer(1, Linux, true);
-    Computer(2, Linux, false);
-    Computer(3, MacOS, false);
-    Computer(4, Windows, false);
-    Computer(5, MacOS, false);
-    Computer(6, Linux, true);
-    Computer(7, Other, false);
-    Computer(8, Other, false)]
+    let osProb = Map [ ("Windows", 0.7); ("Linux", 0.4); ("MacOS", 0.5); ("Other", 0.9)]
+
+    let computers = [Computer(0, "Windows", false, osProb);
+    Computer(1, "Linux", true, osProb);
+    Computer(2, "Linux", false, osProb);
+    Computer(3, "MacOS", false, osProb);
+    Computer(4, "Windows", false, osProb);
+    Computer(5, "MacOS", false, osProb);
+    Computer(6, "Linux", true, osProb);
+    Computer(7, "Other", false, osProb);
+    Computer(8, "Other", false, osProb)]
     
     let connection = Dictionary<int, int list>()
     connection.Add(0, [2; 5; 8])
@@ -30,7 +32,7 @@ let Test1 () =
 
     let finalState =  [true; true; true; true; true; true; true; false; true]
 
-    let net = Network(computers, connection)
+    let net = Network(computers, connection, osProb)
     let rec loop acc =
         if acc = 50 then net.IsFinished()
         else
@@ -42,35 +44,35 @@ let Test1 () =
 
 [<Test>]
 let TestWithZeroProb () =
-    let computers = [Computer(0, Windows, false);
-    Computer(1, Linux, true);
-    Computer(2, Linux, false);
-    Computer(3, MacOS, false);]
+    let osProb = Map [ ("Windows", 0.0); ("Linux", 0.0); ("MacOS", 0.0); ("other", 0.0)]
+    let computers = [Computer(0, "Windows", false, osProb);
+    Computer(1, "Linux", true, osProb);
+    Computer(2, "Linux", false, osProb);
+    Computer(3, "MacOS", false, osProb);]
     
     let connection = Dictionary<int, int list>()
     connection.Add(0, [1])
     connection.Add(1, [2; 0])
     connection.Add(2, [3; 1])
     connection.Add(3, [2])
-
     let finalState =  [false; true; false; false]
 
-    let net = Network(computers, connection)
-    net.SetProbability(0, 0, 0, 0)
+    let net = Network(computers, connection, osProb)
     let rec loop acc =
         if acc = 10 then net.IsFinished()
         else
             net.DoStep() |> ignore
             loop (acc + 1)
     (loop 0) |> should equal true
-    net.Computers |> List.filter (fun comp -> comp.IsInfected <> finalState[comp.Index]) |> should be Empty
+    net.Computers |> List.map (fun comp -> comp.IsInfected) |> should equal finalState
 
 [<Test>]
 let TestWithHundredProb () =
-    let computers = [Computer(0, Windows, false);
-    Computer(1, Linux, true);
-    Computer(2, Linux, false);
-    Computer(3, MacOS, false);]
+    let osProb = Map [ ("Windows", 1.0); ("Linux", 1); ("MacOS", 1); ("other", 1)]
+    let computers = [Computer(0, "Windows", false, osProb);
+    Computer(1, "Linux", true, osProb);
+    Computer(2, "Linux", false, osProb);
+    Computer(3, "MacOS", false, osProb);]
     
     let connection = Dictionary<int, int list>()
     connection.Add(0, [1])
@@ -80,8 +82,7 @@ let TestWithHundredProb () =
 
     let finalState =  [true; true; true; true]
 
-    let net = Network(computers, connection)
-    net.SetProbability(1, 1, 1, 1)
+    let net = Network(computers, connection, osProb)
     net.DoStep() |> ignore
     net.IsFinished() |> should equal true
     net.Computers |> List.filter (fun comp -> comp.IsInfected <> finalState[comp.Index]) |> should be Empty
@@ -93,8 +94,10 @@ let TestWithNoNet () =
     let connection = Dictionary<int, int list>()
 
     let finalState =  []
+    
+    let osProb = Map [ ("Windows", 1.0); ("Linux", 0); ("MacOS", 0.5); ("other", 1)]
 
-    let net = Network(computers, connection)
+    let net = Network(computers, connection, osProb)
     net.DoStep() |> ignore
     net.IsFinished() |> should equal true
     net.Computers |> List.filter (fun comp -> comp.IsInfected <> finalState[comp.Index]) |> should be Empty
--- hw-5/task-1/Computer.fs
+++ hw-5/task-1/Computer.fs
@@ -2,43 +2,35 @@
 open System
 open System.Collections.Generic
 
-type OperationSystem =
-    | Windows
-    | Linux 
-    | MacOS
-    | Other
 
-let mutable prob = Map [ ("winProb", 0.7); ("linProb", 0.4); ("macProb", 0.5); ("otherProb", 0.9)]
+//let mutable prob = Map [ ("winProb", 0.7); ("linProb", 0.4); ("macProb", 0.5); ("otherProb", 0.9)]
+let getProbability (os:string, probs:Map<string, float>) =
+    match (probs.TryGetValue os) with 
+    | (res, prob) when res = true -> prob
+    | _ -> 1
 
-let setProb(win, lin, mac, oth) =
-    Map [("winProb", win); ("linProb", lin); ("macProb", mac); ("otherProb", oth)]
+type Computer(index: int, os: string, hasVirus: bool, osProbs:Map<string, float>) =
+        let mutable HasVirus = hasVirus
 
-let getProbability (os:OperationSystem) =
-    match os with
-    | Windows -> prob["winProb"]
-    | Linux -> prob["linProb"]
-    | MacOS -> prob["macProb"]
-    | _ -> prob["otherProb"]
+        let tryInfect (osProbs:Map<string, float>) =
+            let rnd = (Random().Next(0, 100):float) / 100.0
+            let prob = getProbability(os, osProbs)
+            if (prob <> 0 && rnd <= prob && not HasVirus) then 
+                HasVirus <- true
 
-type Computer(index: int, os: OperationSystem, hasVirus: bool) =
-    let mutable HasVirus = hasVirus
-
-    let tryInfect () =
-        let rnd = (Random().Next(0, 100):float) / 100.0
-        let prob = getProbability os
-        if (prob <> 0 && rnd <= prob && not HasVirus) then 
-            HasVirus <- true
-
-    member _.OS = os
-    member _.Print = $"%A{os}: %b{HasVirus} "
-    member _.TryInfect = tryInfect()
-    member _.IsInfected = HasVirus
-    member _.Index = index
+        member _.OS = os
+        member _.Print = $"%A{os}: %b{HasVirus} "
+        member _.TryInfect = tryInfect(osProbs)
+        member _.IsInfected = HasVirus
+        member _.Index = index
     
-    new(index, os: OperationSystem) = Computer(index, os, false)
+        new(index, os: string, osProbs) = Computer(index, os, false, osProbs)
 
- type Network (computers:Computer list, connection: Dictionary<int, int list>) =
+ type Network (computers:Computer list, connection: Dictionary<int, int list>, OSProbs: Map<string, float>) =
+    let osProbs = OSProbs
+    
     let mutable isFinished = false
+
     let infect (baseComp: Computer) = 
         if baseComp.IsInfected then
             connection.[baseComp.Index] |> 
@@ -48,7 +40,7 @@ type Computer(index: int, os: OperationSystem, hasVirus: bool) =
     let checkComp(baseComp: Computer) = 
         if baseComp.IsInfected then
             connection.[baseComp.Index] |> 
-            List.filter (fun i -> (not computers[i].IsInfected && getProbability(computers[i].OS) <> 0)) = list.Empty
+            List.filter (fun i -> (not computers[i].IsInfected && getProbability(computers[i].OS, osProbs) <> 0)) = list.Empty
         else 
             true
 
@@ -59,9 +51,6 @@ type Computer(index: int, os: OperationSystem, hasVirus: bool) =
 
     member _.DoStep () = 
         computers |> List.map (fun comp -> infect comp) |> ignore
-        
-    member _.SetProbability(win, lin, mac, oth) =
-        prob <- setProb(win, lin, mac, oth)
 
     member _.IsFinished () = 
         CheckFinish()
